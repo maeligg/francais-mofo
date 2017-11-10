744e948b25e3307535f57f28b0d2b089e816a100
Maël Brunet <mbt@emakina.com>
2017-11-10 18:03:50 +0100

    Préparation pour le déploiement

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const Twit = require('twit');
 const anglicismes = require('./anglicismes.json');
 // Uncomment these lines if running locally (see readme for more details)
-const config = require('./config.js');
-process.env = config;
+// const config = require('./config.js');
+// process.env = config;
 
-
-// Utils
-const randomItem = array => array[Math.floor(Math.random() * array.length)];
 
 const T = new Twit({
   consumer_key: process.env.consumer_key,
@@ -31,33 +28,31 @@ const tweet = () => {
       // Loop through all anglicismes
       Object.keys(anglicismes).forEach((anglicismeKey) => {
         // If the tweet contains an anglicisme, save it along with the anglicisme in an array
-        // @TODO avoid overwriting regex, selectedTweet and selectedAnglicisme on each new match.
+        // @TODO avoid updating regex, selectedTweet and selectedAnglicisme on each new match.
         regex = new RegExp(`\\b${anglicismeKey}\\b`, 'g');
 
-        if(regex.test(statuses[statusKey].text)) {
+        if (regex.test(statuses[statusKey].text)) {
           selectedTweet = statuses[statusKey];
           selectedAnglicisme = anglicismeKey;
 
           const tweetId = selectedTweet.id_str;
           const username = selectedTweet.user.screen_name;
           const tweetContent = `Plutôt que « ${selectedAnglicisme} », pourquoi ne pas utiliser « ${anglicismes[selectedAnglicisme]} » ?`;
-          console.log(selectedTweet.text, tweetContent);
 
-          // T.post('statuses/update', { status: `@${username} ${tweetContent}`, in_reply_to_status_id: tweetId }, (postErr, postData) => {
-          //   if (postErr) {
-          //     console.log('error: ', postErr);
-          //   } else {
-          //     console.log('response: ', postData);
-          //   }
-          // });
+          T.post('statuses/update', { status: `@${username} ${tweetContent}`, in_reply_to_status_id: tweetId }, (postErr, postData) => {
+            if (postErr) {
+              console.log('error: ', postErr);
+            } else {
+              console.log('response: ', postData);
+            }
+          });
         }
       });
     });
 
     // If no result was found, expand the results and search again (within a limit of 1000)
     if (!selectedTweet && numResults <= 1000) {
-      console.log(numResults)
-      numResults +=100;
+      numResults += 100;
       tweet();
     }
   });
